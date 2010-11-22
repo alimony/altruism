6332d22f57690abaf44ed0fd8717a9e9ec5ada1d
markus.magnuson <markus.magnuson@36c5233c-e8d5-3d18-309e-52157e2a7df6>
2010-11-22 01:46:36 +0000

    Add 1.0 and 1.1 tags from old repo.

--- ASMSoulseekPathProvider.m
+++ ASMSoulseekPathProvider.m
@@ -1,23 +1,21 @@
 /*
-Copyright (c) 2008 Markus Amalthea Magnuson <markus.magnuson@gmail.com>
+-----------------------------------------------
+  ALTRUISM LICENSE
+ 
+  “Do what you want to do,
+  and go where you're going to
+  Think for yourself,
+  'cause I won't be there with you”
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+  You are completely free to do anything with
+  this source code, but if you try to make
+  money on it you will be beaten up with a
+  large stick. I take no responsibility for
+  anything, and this license text must
+  always be included.
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
+  Markus Amalthea Magnuson <markus.magnuson@gmail.com>
+-----------------------------------------------
 */
 
 #import <AGRegex/AGRegex.h>
@@ -37,74 +35,15 @@ THE SOFTWARE.
 
 - (void)awakeFromNib
 {
-	CFStringRef ssXBundleId = CFSTR("net.schleifer.chris.ssX");
+	// TODO: Hur låta användaren välja path? Prefsfönster?
+	NSString *logfilePath = [@"~/Library/Logs/ssX/ssX_Console.log" stringByExpandingTildeInPath];
 	
-	// let's read the logfile path from ssX's prefs
-	CFPropertyListRef logfileDirectory = CFPreferencesCopyAppValue(CFSTR("ssXLogLogDirectory"), ssXBundleId);
-	
-	if (logfileDirectory && CFGetTypeID(logfileDirectory) == CFStringGetTypeID()) {
-		// logfile is pretty useless if ssX isn't set up properly, so let's check that
-		CFPropertyListRef value1 = CFPreferencesCopyAppValue(CFSTR("ssXLogLogConsole"), ssXBundleId);
-		CFPropertyListRef value2 = CFPreferencesCopyAppValue(CFSTR("ssXLogLevelTransfer"), ssXBundleId);
-		
-		BOOL ssXLogConsoleMessages = (value1 == kCFBooleanTrue);
-		BOOL ssXTransferActicity = (value2 == kCFBooleanTrue);
-		
-		if (value1)
-			CFRelease(value1);
-		if (value2)
-			CFRelease(value2);
-		
-		if (!ssXLogConsoleMessages || !ssXTransferActicity) {
-			// the proper prefs weren't set, so ask the user to do so automatically
-			NSAlert *ssXPreferencesAlert = [NSAlert alertWithMessageText:@"Incorrect preferences"
-														   defaultButton:@"OK"
-														 alternateButton:@"Cancel"
-															 otherButton:nil
-											   informativeTextWithFormat:@"You need to check the boxes “Log console messages” and “Transfer Activity” in the section “Logs” of the ssX preferences window. Do you want me to do this for you?"];
-			
-			if ([ssXPreferencesAlert runModal] == NSAlertDefaultReturn) {
-				// make the needed changes to ssX prefs
-				CFPreferencesSetAppValue(CFSTR("ssXLogLogConsole"), [NSNumber numberWithBool:YES], ssXBundleId);
-				CFPreferencesSetAppValue(CFSTR("ssXLogLevelTransfer"), [NSNumber numberWithBool:YES], ssXBundleId);
-				
-				CFPreferencesAppSynchronize(ssXBundleId);
-				
-				// TODO: Restart ssX automatically, I haven't devised a clean way to do this yet
-				NSAlert *restartAlert = [NSAlert alertWithMessageText:@"Restart"
-														defaultButton:@"OK"
-													  alternateButton:nil
-														  otherButton:nil
-											informativeTextWithFormat:@"You need to restart ssX for changes to take effect."];
-				
-				[restartAlert runModal];
-				
-				// everything should be set up properly now
-				ssXLogConsoleMessages = YES;
-				ssXTransferActicity = YES;
-			}
-		}
-		
-		// now get to business
-		if (ssXLogConsoleMessages && ssXTransferActicity) {
-			NSString *logfilePath = [(NSString *)logfileDirectory stringByAppendingPathComponent:@"ssX_Console.log"];
-			NSLog(@"Trying logfile at %@", logfilePath);
-			BOOL succeeded = [super tailThisForMe:logfilePath];
-			
-			if (succeeded) {
-				onlyUploadsRegexp = [[AGRegex alloc] initWithPattern:@"Finished uploading (.*) to user"];
-			}
-			else {
-				NSLog(@"Couldn't open logfile at %@", logfilePath);
-			}
-		}
-	}
-	else {
-		NSLog(@"Couldn't read ssX preferences, you may need to run ssX once before running Altruism");
-	}
-	
-	if (logfileDirectory)
-		CFRelease(logfileDirectory);
+	// return early (and make this path provider do nothing) if no log file was found.
+	if ([[NSFileManager defaultManager] fileExistsAtPath:logfilePath] == NO)
+		return;
+    
+	onlyUploadsRegexp = [[AGRegex alloc] initWithPattern:@"Finished uploading (.*) to user"];
+	BOOL succeeded = [super tailThisForMe:logfilePath];
 }
 
 - (void)dealloc
